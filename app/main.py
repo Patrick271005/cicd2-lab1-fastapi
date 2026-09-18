3534d673c7926dc590846c20711c6c429aa0aebc
Patrick271005 <150245262+Patrick271005@users.noreply.github.com>
2026-09-18 10:55:24 +0000

    Added route for making a user

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, HTTPException, status
+from app.schemas import UserCreate
 app = FastAPI(title="Lab1 - FastAPI User API")
 
+users: list[UserCreate] = []
 @app.get("/health")
 def health(): 
     return {"status": "ok"}
@@ -9,3 +10,7 @@ def health():
 @app.get("/hello")
 def hello(): 
     return {"message": "Hello World"}
+@app.post("/app/users", status_code=status.HTTP_201_CREATED)
+def add_user(new_user: UserCreate):
+    users.append(new_user)
+    return new_user
