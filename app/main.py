253b5b695fa31088116065cecabf89e24254279c
Patrick271005 <150245262+Patrick271005@users.noreply.github.com>
2026-09-18 11:20:45 +0000

    Implemented get user route and commented out delete function

--- app/main.py
+++ app/main.py
@@ -19,3 +19,29 @@ def add_user(new_user: UserCreate):
                                 detail="A user with this id already exists")
     users.append(new_user)
     return new_user
+@app.get("/api/users")
+def get_users():
+    return users
+
+@app.get("/api/users/{user_id}")
+def get_user(user_id: int):
+    for existing_user in users:
+        if existing_user.userid == user_id:
+            return existing_user
+
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        details="User not found",
+    )
+
+
+
+
+#@app.delete("/api/users/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
+#def delete_user(user_id: int):
+    # Use enumerate so we can find the user and remove it by index.
+    #for index, existing_user in enumerate(users):
+        #if existing_user.user_id == user_id:
+            #users.pop(index)
+        #return Response(status_code=status.HTTP_204_NO_CONTENT)
+#raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="User not found",)
